22813b96d1c25c7f611e7c488059ab8f13554d33
Manuel Pineyro <manuel@simplebet.io>
2023-04-25 19:46:40 -0400

    edits

--- microk8s/setup.sh
+++ microk8s/setup.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
+# Function to add a line to a file if it doesn't already exist
+add_line_to_file() {
+  local line="$1"
+  local file="$2"
+
+  grep -qxF "$line" "$file"
+
+  if [ $? -ne 0 ]; then
+    echo "Adding line to $file:"
+    echo "$line"
+    echo "$line" >> "$file"
+  else
+    echo "Line already exists in $file:"
+    echo "$line"
+  fi
+}
+
 # Configure microk8s permissions
 sudo usermod -a -G microk8s $(whoami)
-microk8s config > \$HOME/.kube/config
+
+# Create/update the .kube/config file with MicroK8s configuration if it doesn't exist or if it doesn't contain MicroK8s configuration
+if [ ! -f $HOME/.kube/config ] || ! grep -q "microk8s-cluster" $HOME/.kube/config; then
+    echo "Updating $HOME/.kube/config with MicroK8s configuration"
+    microk8s config > $HOME/.kube/config
+else
+    echo "$HOME/.kube/config already contains MicroK8s configuration"
+fi
 sudo chown -R suspiro ~/.kube
 newgrp microk8s
 
 # Create an alias for kubectl to use microk8s kubectl
-echo "alias kubectl='microk8s kubectl'" >> \$HOME/.bash_aliases
-echo "alias helm='microk8s.helm'" >> \$HOME/.bash_aliases
+add_line_to_file "alias kubectl='microk8s kubectl'" "$HOME/.bash_aliases"
+add_line_to_file "alias helm='microk8s.helm'" "$HOME/.bash_aliases"
 
 # Enable required MicroK8s add-ons
-microk8s enable dns helm3 dashboard 
+microk8s enable dns helm3 dashboard storage community traefik
 
 # Source aliases
 . \$HOME/.bash_aliases
--- setup.sh
+++ setup.sh
@@ -1,14 +1,62 @@
 #!/bin/bash
 
-# Install Nix
-curl -L https://nixos.org/nix/install | sh
+# # Install Nix
+# curl -L https://nixos.org/nix/install | sh
 
-# Add Nix to the shell environment
-. \$HOME/.nix-profile/etc/profile.d/nix.sh
+# # Add Nix to the shell environment
+# . \$HOME/.nix-profile/etc/profile.d/nix.sh
 
-# Enable Nix experimental features
-mkdir -p \$HOME/.config/nix
-echo "experimental-features = nix-command flakes" >> \$HOME/.config/nix/nix.conf
+# # Enable Nix experimental features
+# mkdir -p \$HOME/.config/nix
+# echo "experimental-features = nix-command flakes" >> \$HOME/.config/nix/nix.conf
 
 # Install MicroK8s using snap
 sudo snap install microk8s --classic
+
+# Install flux for GitOps
+curl -s https://fluxcd.io/install.sh | sudo bash
+
+# Install yq
+sudo snap install yq
+
+# Check if the yq command is available
+if ! command -v yq &> /dev/null; then
+  echo "Error: yq is not installed. Install it using 'sudo snap install yq'"
+  exit 1
+fi
+
+# Define the YAML file containing the environment variables
+YAML_FILE="env_vars.yaml"
+
+# Function to add an environment variable to ~/.bashrc if it doesn't already exist
+add_env_var() {
+  local name="$1"
+  local value="$2"
+
+  grep -qxF "export $name=$value" ~/.bashrc
+
+  if [ $? -ne 0 ]; then
+    echo "Adding environment variable $name to ~/.bashrc"
+    echo "export $name=$value" >> ~/.bashrc
+  else
+    echo "Environment variable $name already exists in ~/.bashrc"
+  fi
+}
+
+# Read environment variables from the YAML file and add them to ~/.bashrc
+yq eval '. as $o ireduce ({}; . * $o)' "$YAML_FILE" | while IFS="=" read -r env_var_name env_var_value; do
+  add_env_var "$env_var_name" "$env_var_value"
+done
+
+# Source the .bashrc file to make the environment variables available immediately
+source ~/.bashrc
+
+chmod +x mircok8s/setup.sh
+sh mircok8s/setup.sh
+
+# Bootstrap flux
+flux bootstrap github \
+  --owner=\$GH_USER \ 
+  --repository=\$FLUX_REPOSITORY \
+  --personal \
+  --path bootstrap
