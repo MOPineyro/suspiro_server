35b12ffd2df23589ccd8555a6f4b8a02c713c9c9
Manuel Pineyro <manuel@simplebet.io>
2023-04-25 22:41:48 -0400

    more edits

--- setup.sh
+++ setup.sh
@@ -44,7 +44,10 @@ add_env_var() {
 }
 
 # Read environment variables from the YAML file and add them to ~/.bashrc
-yq eval '. as $o ireduce ({}; . * $o)' "$YAML_FILE" | sed 's/.$//' | while IFS="=" read -r env_var_name env_var_value; do
+yq e '. as $k=>$v | "export " + ($k|tojson) + "=" + ($v|tojson)' "$YAML_FILE" | while IFS="=" read -r env_var_name env_var_value; do
+  # Remove the quotes from the env_var_name and env_var_value
+  env_var_name=$(echo "$env_var_name" | tr -d '"')
+  env_var_value=$(echo "$env_var_value" | tr -d '"')
   add_env_var "$env_var_name" "$env_var_value"
 done
 
