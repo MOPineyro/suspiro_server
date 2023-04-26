f5169610266e14fd64b5e84f3f5163df1330d20c
Manuel Pineyro <manuel@simplebet.io>
2023-04-25 22:55:00 -0400

    more edits

--- setup.sh
+++ setup.sh
@@ -15,45 +15,7 @@ sudo snap install microk8s --classic
 
 # Install flux for GitOps
 curl -s https://fluxcd.io/install.sh | sudo bash
-
-# Install yq
-sudo snap install yq
-
-# Check if the yq command is available
-if ! command -v yq &> /dev/null; then
-  echo "Error: yq is not installed. Install it using 'sudo snap install yq'"
-  exit 1
-fi
-
-# Define the YAML file containing the environment variables
-YAML_FILE="env_vars.yaml"
-
-# Function to add an environment variable to ~/.bashrc if it doesn't already exist
-add_env_var() {
-  local name="$1"
-  local value="$2"
-
-  grep -qxF "export $name=$value" ~/.bashrc
-
-  if [ $? -ne 0 ]; then
-    echo "Adding environment variable $name to ~/.bashrc"
-    echo "export $name=$value" >> ~/.bashrc
-  else
-    echo "Environment variable $name already exists in ~/.bashrc"
-  fi
-}
-
-# Read environment variables from the YAML file and add them to ~/.bashrc
-yq e '. as $k=>$v | "export " + ($k|tojson) + "=" + ($v|tojson)' "$YAML_FILE" | while IFS="=" read -r env_var_name env_var_value; do
-  # Remove the quotes from the env_var_name and env_var_value
-  env_var_name=$(echo "$env_var_name" | tr -d '"')
-  env_var_value=$(echo "$env_var_value" | tr -d '"')
-  add_env_var "$env_var_name" "$env_var_value"
-done
-
-# Source the .bashrc file to make the environment variables available immediately
-source ~/.bashrc
-
+sh ./setup/add_env_vars.sh
 sh ./microk8s/setup.sh
 
 # Bootstrap flux
